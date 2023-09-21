442f30e17d43c2441d28c6c110a6867c1f6b5266
d.fathi <d.fathi@kian.digital>
2023-09-22 00:17:47 +0330

    create spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="w-full h flex items-center justify-center">
+      <div class="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-7 w-7"></div>
+    </div>
+  );
+};
+
+export default Spinner;
